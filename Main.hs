b05bfb8d73737cdada3a69327fb288d05f951382
micheleberetta98 <beretta.m98@gmail.com>
2022-02-25 20:19:51 +0100

    easier

--- Main.hs
+++ Main.hs
@@ -1,19 +1,17 @@
 module Main where
 
-import           Data.List   (transpose)
-import           Data.Maybe  (isJust, isNothing)
-import           Data.Vector (Vector, (!), (//))
-import qualified Data.Vector as V
+import           Data.List  (transpose)
+import           Data.Maybe (isJust, isNothing)
 
-type Board = Vector Row
-type Row   = Vector (Maybe Player)
+type Board = [Row]
+type Row   = [Maybe Player]
 
 data Player = X | O
   deriving (Eq)
 
 main :: IO ()
 main = do
-  let emptyBoard = V.replicate 3 (V.replicate 3 Nothing)
+  let emptyBoard = replicate 3 (replicate 3 Nothing)
       count = length (allPlays X emptyBoard)
   putStr "All possible (valid) games: " >> print count
   putStr "Expected:                   " >> print 255168
@@ -21,7 +19,7 @@ main = do
 allPlays :: Player -> Board -> [Board]
 allPlays player board = do
   spot <- freeSpots board
-  let board' = play board player spot
+  let board' = move player spot board
   if isFull board' || isWin board'
     then pure board'
     else allPlays (nextPlayer player) board'
@@ -30,18 +28,15 @@ isFull :: Board -> Bool
 isFull = all (notElem Nothing)
 
 isWin :: Board -> Bool
-isWin board = any (any allSame) [board', transpose board', diags board']
+isWin board = any (any allSame) [diags board, board, transpose board]
   where
-    board' = map V.toList $ V.toList board
     allSame [a,b,c] = isJust a && a == b && b == c
     allSame _       = False
     diags [[a,_,a'], [_,b,_], [c',_,c]] = [[a,b,c], [a',b,c']]
     diags _                             = []
 
-play :: Board -> Player -> (Int, Int) -> Board
-play board player (x, y) = board // [(x, row')]
-  where
-    row' = (board ! x) // [(y, Just player)]
+move :: Player -> (Int, Int) -> Board -> Board
+move player (x, y) = update x $ update y (const $ Just player)
 
 nextPlayer :: Player -> Player
 nextPlayer X = O
@@ -49,8 +44,8 @@ nextPlayer O = X
 
 freeSpots :: Board -> [(Int, Int)]
 freeSpots board = filter isFree ((,) <$> [0..2] <*> [0..2])
-  where isFree (i, j) = isNothing (board ! i ! j)
+  where isFree (i, j) = isNothing (board !! i !! j)
 
-updateList :: [a] -> a -> Int -> [a]
-updateList lst value i = zipWith update [0..] lst
-  where update j x = if i == j then value else x
+update :: Int -> (a -> a) -> [a] -> [a]
+update i f = zipWith update [0..]
+  where update j x = if i == j then f x else x
